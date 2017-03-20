e9a5e401d9a0ed8c62482a7eaae1d95edef94ba1
寺沢 淳 <vfr400nc30@hotmail.com>
2017-03-20 22:24:22 +0900

    初回

---
+++ ConsoleApp1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.Out.Write("Hello World\n");
+
+            Console.Out.Write("Visual Stidio 2017\n");
+
+
+        }
+    }
+}
